0e840d2aded5e7880ec32ee780a2c442df866a89
Yosua Nicolaus <cryodawn257@gmail.com>
2022-04-26 00:41:55 +0700

    add cat images & cat mode

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { Card } from "./Card";
 import { ScoreBoard } from "./ScoreBoard";
 import { StaticModal } from "./StaticModal";
 
-let images = [
+const dogImages = [
   { path: "./assets/dog0.jpg", name: "John Doe" },
   { path: "./assets/dog1.jpg", name: "Bob Doe" },
   { path: "./assets/dog2.jpg", name: "Alex Doe" },
@@ -16,10 +16,28 @@ let images = [
   { path: "./assets/dog7.jpg", name: "Doe Brenoe" },
 ];
 
+const catImages = [
+  { path: "./assets/cat0.jpg", name: "Larry M Greene" },
+  { path: "./assets/cat1.jpg", name: "Cheryl D Harvey" },
+  { path: "./assets/cat2.jpg", name: "Madge P Jessee" },
+  { path: "./assets/cat3.jpg", name: "Gary J Mulligan" },
+  { path: "./assets/cat4.jpg", name: "Robert R Lewis" },
+  { path: "./assets/cat5.jpg", name: "Deneen J Murphy" },
+  { path: "./assets/cat6.jpg", name: "Shirley K Rishel" },
+  { path: "./assets/cat7.jpg", name: "Joseph R Bland" },
+  { path: "./assets/cat8.jpg", name: "Kelly F Mulkey" },
+  { path: "./assets/cat9.jpg", name: "Jamila E Medlock" },
+  { path: "./assets/cat10.jpg", name: "Robert A Ward" },
+  { path: "./assets/cat11.jpg", name: "Christian E Johnson" },
+];
+
+let images = dogImages;
+
 function App() {
   const [data, setData] = useState([]);
   const [score, setScore] = useState(0);
   const [order, setOrder] = useState(images);
+  const [mode, setMode] = useState("Dog");
 
   const updateData = (newData) => {
     if (data.includes(newData)) {
@@ -31,6 +49,19 @@ function App() {
       setScore(score + 1);
     }
     setOrder(shuffled(images));
+  };
+
+  const changeMode = () => {
+    if (mode === "Dog") {
+      setMode("Cat");
+      images = catImages;
+    } else {
+      setMode("Dog");
+      images = dogImages;
+    }
+    setOrder(shuffled(images));
+    setData([]);
+    setScore(0);
   };
 
   useEffect(() => {
@@ -57,8 +88,8 @@ function App() {
         </div>
       </div>
       <div className="d-flex justify-content-center pb-5 gap-2">
-        <button className="btn btn-dark" onClick={() => console.log(data)}>
-          nice button
+        <button className="btn btn-dark" onClick={changeMode}>
+          {mode === "Dog" ? "Cat" : "Dog"} mode
         </button>
         <StaticModal />
       </div>
