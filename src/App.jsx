62f0bb85a545ba063b8fb991bde882f57b160f77
Yosua Nicolaus <cryodawn257@gmail.com>
2022-04-25 10:03:10 +0700

    initialize project with React and Bootstrap
    deleted abundant files
    added custom theme color from colormind.io

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="display-1">Hello world</div>
+    </div>
+  );
+}
+
+export default App;
